93e448fdbf66f20d138b2bf1843528e212d914b9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-04-30 14:35:38 +0200

    Add compatibility with stock_dynamic_routing
    in stock_move_common_dest_sync_location
    
    Some notes:
    
    * syncing the destinations could not be done during _action_assign: if
    we call _action_done on several moves at once with different locations,
    we don't expect them to change of destination under the hood! So we sync
    after _action_done
    * For now, no code is needed in stock_dynamic_routing_common_dest_sync
    to make the modules compatibles, but it contains tests with routing +
    sync

---
+++ stock_dynamic_routing_common_dest_sync/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Camptocamp (https://www.camptocamp.com)
+{
+    "name": "Stock Routing Operations - Dest. Sync",
+    "summary": "Glue module",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        # FIXME will be renamed to stock_dynamic_routing
+        "stock_routing_operation",
+        "stock_move_common_dest_sync_location",
+    ],
+    "demo": [],
+    "data": [],
+    "auto_install": True,
+    "installable": True,
+    "development_status": "Alpha",
+}
